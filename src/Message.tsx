fecb23c2c8cfa86cdd048497d19f6d1d3bf09952
limzisoon <limzisoon@hotmail.com>
2023-11-21 14:13:49 +0800

    1st commit

---
+++ src/Message.tsx
@@ -0,0 +1,14 @@
+
+//Pascal Casing
+function Message(){
+
+    //JXS javascript XML
+    const name = 'Zi Soon';
+    
+    if(name)
+        return <h1>Hello {name}</h1>; 
+    
+    return <h1>Hello World</h1>; 
+}
+
+export default Message;
